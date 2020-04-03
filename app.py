3546c8c80a985c1230eba29473ec0ea963f489bc
vanderandrade <vander_c_andrade@hotmail.com>
2020-04-03 09:26:11 -0300

    Added create bucket function on Rest API

--- app.py
+++ app.py
@@ -10,9 +10,20 @@ class S3RestAPI(Resource):
     def get(self, action: str):
         try:
             print(request.url)
-            if action == 'get':
+            if action == 'getAllBuckets':
                 return [bucket['Name'] for bucket in aws.listAllS3Buckets()]
             return 'Action not found!'
+        except:
+            return 'Not ok'
+        
+    def post(self, action: str):
+        try:
+            if action == 'createBucket':
+                bucketName = request.form['bucket_name']
+                region = request.form ['region'] if 'region' in request.form else None
+                aws.createS3Bucket(bucketName, region)
+            
+            return 'Ok'
         except:
             return 'Not ok'
 
@@ -25,13 +36,7 @@ class EC2RestAPI(Resource):
             return {'Error':'Not ok'}
 
 api.add_resource(EC2RestAPI, '/ec2/<string:action>/', methods=['POST']) # methods=['GET', 'POST']
-api.add_resource(S3RestAPI, '/s3/<string:action>/', methods=['GET'])
+api.add_resource(S3RestAPI, '/s3/<string:action>/', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='3000')
-
-# class RESTapp(Resource):
-#     @staticmethod
-#     def get(path=''):  # <-- You should provide the default here not in api.add_resource()!
-#         return 'You want path: \'%s\'' % path  # do get something
-# api.add_resource(RESTapp, '/', '/<path:path>')
--- test.py
+++ test.py
@@ -4,8 +4,14 @@ host='0.0.0.0'
 port='3000'
 _ec2_machine_id = ''
 
-_response = get(f'http://{host}:{port}/s3/get/').json()
+_response = get(f'http://{host}:{port}/s3/getAllBuckets/').json()
 print(f'S3 Buckets: {_response}')
 
 _response2 = post(f'http://{host}:{port}/ec2/stop/', data={'instance_id': _ec2_machine_id}).json()
 print(f'Turn off EC2 machine: {_response2}')
+
+# # # # # # # # # # # # # # # # # # #
+bucket_name = ''
+_response3 = post(f'http://{host}:{port}/s3/createBucket/', data={'bucket_name': bucket_name}).json()
+print(f'Turn off EC2 machine: {_response3}')
+# # # # # # # # # # # # # # # # # # #
